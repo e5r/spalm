6201de43d9d732d101399bbdc19ebc104c4542ca
Erlimar Silva Campos <erlimar@gmail.com>
2020-07-06 14:21:44 -0300

    Adiciona suporte StructOpt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use spalm::cli::Cmd;
 use spalm::cli::commands::InitCommand;
 
-fn main() {
-    let init = InitCommand::new();
+use structopt::StructOpt;
 
-    match init.exec() {
-        Ok(_) => std::process::exit(0),
-        Err(code) => std::process::exit(code)
-    }
+#[derive(Debug, StructOpt)]
+#[structopt(name="spalm", version="0.1.0", about = "Specification Project of E5R Application Lifecycle Management")]
+struct Opt {}
+
+fn main() {
+    // let init = InitCommand::new();
+
+    // match init.exec() {
+    //     Ok(_) => std::process::exit(0),
+    //     Err(code) => std::process::exit(code)
+    // }
+
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
